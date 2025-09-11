f2af274de296ec99a392d8fd0c22cc57c09558fe
aditya <adityapiyush71@gmail.com>
2025-09-11 23:21:01 +0530

    deleted old backend

--- backend/src/data/routes.js
+++
@@ -1,8 +0,0 @@
-const routes = [
-  { id: "R1", name: "Chandigarh – Mohali – Zirakpur" },
-  { id: "R2", name: "Ludhiana – Jalandhar – Phagwara" },
-  { id: "R3", name: "Amritsar – Batala – Gurdaspur" },
-  { id: "R4", name: "Patiala – Rajpura – Banur" }
-];
-
-module.exports = { routes };
